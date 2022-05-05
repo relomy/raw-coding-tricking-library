e24e1b2f0788c6a4381d91044ace666e8b76a5ab
Adam Lewandowski <relomy@gmail.com>
2022-05-05 13:58:09 -0400

    init

---
+++ TrickingLibrary.Api/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace TrickingLibrary.Api;
+
+public class Startup
+{
+    private const string AllCors = "All";
+    
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        services.AddCors(options => options.AddPolicy(AllCors, build =>
+            build.AllowAnyHeader()
+                .AllowAnyOrigin()
+                .AllowAnyMethod()));
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseCors(AllCors);
+        
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapDefaultControllerRoute();
+        });
+    }
+}
